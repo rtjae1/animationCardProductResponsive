3e4e4b42063b23026ea2b20f2e94d5cd3f91cfac
rtjae1 <ryan.t.jae@gmail.com>
2020-05-11 11:50:56 -0400

    update to start

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+    document.getElementById("t").style.color = "var(--primaryColor)";
+    document.getElementById("f").style.color = "var(--primaryColor)";
+    document.getElementById("l").style.color = "var(--primaryColor)";
+    document.getElementById("i").style.color = "var(--primaryColor)";
+    document.getElementById("g").style.color = "var(--primaryColor)";
+}
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+    document.getElementById("t").style.color = "var(--secondaryColor)";
+    document.getElementById("f").style.color = "var(--secondaryColor)";
+    document.getElementById("l").style.color = "var(--secondaryColor)";
+    document.getElementById("i").style.color = "var(--secondaryColor)";
+    document.getElementById("g").style.color = "var(--secondaryColor)";
+}
